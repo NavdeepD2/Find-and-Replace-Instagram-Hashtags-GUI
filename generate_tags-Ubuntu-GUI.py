48099adaef5f6e433aa910b14934ee26af9145e6
Navdeep Singh <navdeep.d2@gmail.com>
2023-10-08 01:30:01 +0530

    Update generate_tags-Ubuntu-GUI.py

--- generate_tags-Ubuntu-GUI.py
+++ generate_tags-Ubuntu-GUI.py
@@ -1,16 +1,22 @@
 # Written by Navdeep Singh
 # Email: navdeep.d2@gmail.com
 import tkinter as tk
-import pyperclip
+import subprocess
 
 # Function to perform the Find and Replace operation and copy to clipboard
 def replace_and_copy():
     find_text = find_entry.get()
-    replace_text = replace_entry.get().replace(" ", "")
-    #replace_text = replace_entry.get()
+    replace_text = replace_entry.get().replace(" ", "")  # Remove spaces from replace_text
     tags_data = tags_text.get("1.0", "end-1c")  # Get the current text in the tags_text widget
     updated_tags = tags_data.replace(find_text, replace_text)
-    pyperclip.copy(updated_tags)  # Copy the updated tags to the clipboard
+
+    # Use xclip to copy the updated tags to the clipboard
+    try:
+        subprocess.run(['xclip', '-selection', 'clipboard'], input=updated_tags, text=True, check=True)
+        print("Updated text copied to clipboard")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+
     tags_text.delete("1.0", "end")  # Clear the tags_text widget
     tags_text.insert("1.0", updated_tags)  # Insert the updated tags
 
@@ -40,6 +46,9 @@ tags_text.pack()
 # Create the "Replace and Copy" button
 replace_button = tk.Button(root, text="Replace and Copy", command=replace_and_copy)
 replace_button.pack()
+
+# Start the Tkinter main loop
+root.mainloop()
 
 # Start the Tkinter main loop
 root.mainloop()
